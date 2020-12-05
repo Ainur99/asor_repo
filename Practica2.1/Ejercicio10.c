74d9cab352c2df2cf3653899be3ef4d6da705e77
Ainur99 <57950341+Ainur99@users.noreply.github.com>
2020-12-05 16:21:19 +0100

    Add files via upload

---
+++ Practica2.1/Ejercicio10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+int main() {
+   uid_t id;
+   struct passwd *pass;
+   id = getuid();
+   pass = getpwuid(id);
+   printf("uid = %i\n", id);
+   printf("euid = %i\n", geteuid());
+   printf("nombre de usuario: %s\n", pass->pw_name);
+   printf("directorio home: %s\n", pass->pw_dir);
+   printf("descripcion de usuario: %s\n", pass->pw_gecos);
+}
