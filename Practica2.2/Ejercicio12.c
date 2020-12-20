23b8a6ca7473059a9067d1997115863c2f5c1039
Ainur99 <57950341+Ainur99@users.noreply.github.com>
2020-12-20 16:22:53 +0100

    Create Ejercicio12.c

---
+++ Practica2.2/Ejercicio12.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main (int argc, char** argv) {
+
+   int fd;   
+
+   if (argc != 2) {
+      printf("Numero de argumentos invalido. Uso: %s <fichero>\n", argv[0]);
+      return 0;
+   }
+   if ((fd = open(argv[1],O_WRONLY | O_CREAT | O_TRUNC, 0777)) == -1) {
+      printf("Error al abrir %s\n", argv[1]);
+      return 0;
+   }
+   if (dup2(fd,1) == -1) {
+      printf("Error al duplicar\n");
+      close(fd);
+      return 0;
+   }
+   printf("Hello world!\n");
+   printf("Esto es una prueba\n");
+   close(fd);
+   return 0;
+}
